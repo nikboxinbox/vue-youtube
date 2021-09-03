a81d154cad41d3a9d08582fcdb7c822f301eb440
Nik Nik <nikbox88@gmail.com>
2021-09-03 17:11:53 +0300

    first commit tutorial-2

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <component :is="layout">
+      <router-view />
+    </component>
+  </div>
+</template>
+
+<script>
+import EmptyLayout from "@/layouts/EmptyLayout";
+import MainLayot from "@/layouts/MainLayot";
+export default {
+  computed: {
+    layout() {
+      console.log(this.$route.meta);
+      return "empty-layout";
+    },
+  },
+  components: {
+    EmptyLayout,
+    MainLayot,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "~materialize-css/dist/css/materialize.min.css";
+@import "assets/index.css";
+</style>
